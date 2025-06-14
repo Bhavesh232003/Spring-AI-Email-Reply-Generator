c73d00f71f3f41e5d4c9109070895e4e8b5af91c
Bhavesh232003 <bhaveshtrikha23@gmail.com>
2025-06-14 17:44:08 +0530

    Add files via upload

---
+++ EmailRequest.java
@@ -0,0 +1,12 @@
+package com.email.email.writer;
+
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+
+@Data
+
+public class EmailRequest {
+private String Content;
+private String tone; 
+}
